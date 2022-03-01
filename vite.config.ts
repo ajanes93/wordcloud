e63c2a440e09d3eb0bfd2dc085d2bfb1572f66d1
Andres Janes <andres.janes@windsor-telecom.co.uk>
2022-03-01 18:48:44 +0000

    build: add github pages deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/wordcloud/',
     plugins: [
         vue({
             reactivityTransform: true
