d0821225d2b236fa325ea823f3524684a77fd45d
Andres Janes <andres.janes@windsor-telecom.co.uk>
2022-03-01 15:40:15 +0000

    feat: add BaseSpinner component

---
+++ src/components/BaseSpinner.vue
@@ -0,0 +1,19 @@
+<script
+    setup
+    lang="ts"
+>
+const props = withDefaults(
+    defineProps<{
+        size: number
+    }>(),
+    {
+        size: 16
+    }
+)
+</script>
+
+<template>
+    <div class="flex items-center justify-center">
+        <div :class="`w-${props.size} h-${props.size} border-b-2 border-gray-900 rounded-full animate-spin`"></div>
+    </div>
+</template>
