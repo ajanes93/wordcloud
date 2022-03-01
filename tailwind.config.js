85ca2d07ddd313c94545f3117195a7a5ed6d7cae
Andres Janes <andres.janes@windsor-telecom.co.uk>
2022-03-01 15:39:06 +0000

    build: add tailwind safelist for dynamic generated classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,17 @@ module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {}
-    }
+    },
+    safelist: [
+        {
+            pattern: /text-[gray|red|green|blue]/,
+            variants: ['hover']
+        },
+        {
+            pattern: /bg|border-[blue|red|green]/
+        },
+        {
+            pattern: /w-|h-/
+        }
+    ]
 }
