9384ca68b2fade23234cf553aed44c22a1cdf0ec
Andres Janes <andres.janes@windsor-telecom.co.uk>
2022-03-01 15:39:37 +0000

    feat: add BaseAlert component

---
+++ src/components/BaseAlert.vue
@@ -0,0 +1,17 @@
+<script
+    setup
+    lang="ts"
+>
+const props = defineProps<{
+    color: string
+}>()
+</script>
+
+<template>
+    <div
+        :class="`bg-${props.color}-100 border border-${props.color}-400 text-${props.color}-700 px-4 py-3 rounded relative text-center`"
+        role="alert"
+    >
+        <slot></slot>
+    </div>
+</template>
