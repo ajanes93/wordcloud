1c541cdf0e53fe889de05a12faa845654f7eac70
Andres Janes <andres.janes@windsor-telecom.co.uk>
2022-03-01 18:19:21 +0000

    feat: add unit test for BaseList component

---
+++ test/components/BaseList.test.ts
@@ -0,0 +1,4 @@
+import itRendersSlotData from './utils/itRendersSlotData'
+import BaseList from '../../src/components/BaseList.vue'
+
+test('it renders slot data', () => itRendersSlotData(BaseList))
